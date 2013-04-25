f5591e2391125d44d7b0e19d069dc5bb0e3fa889
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-25 08:32:28 +0000

    ToDoリストにequals()を追加

--- Dollar/DollarTest.php
+++ Dollar/DollarTest.php
@@ -12,6 +12,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'Dollar.php';
  * - [ ] amountのプライベート化
  * - [x] Dollarの副作用
  * - [ ] Moneyの丸め処理
+ * - [ ] equals()
  * </pre>
  */
 class DollarTest extends PHPUnit_Framework_TestCase
