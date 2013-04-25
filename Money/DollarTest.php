0d40fc5d84e096ddb60eaf3081416e48cdcda7b4
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-25 09:10:52 +0000

    ToDoリストを別ファイルに移動

--- Money/DollarTest.php
+++ Money/DollarTest.php
@@ -1,23 +1,6 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Dollar.php';
 
-/**
- * Dollarクラスのテストコード。
- *
- * <pre>
- * ToDo
- * ====
- * - [ ] 2:1の場合 $5 + 10CHF = $10
- * - [x] $5 * 2 = $10
- * - [x] amountのプライベート化
- * - [x] Dollarの副作用
- * - [ ] Moneyの丸め処理
- * - [x] equals()
- * - [ ] hashCode()
- * - [ ] nullとの等価性
- * - [ ] オブジェクトとの等価性
- * </pre>
- */
 class DollarTest extends PHPUnit_Framework_TestCase
 {
     public function testMultiplication()
