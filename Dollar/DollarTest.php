3a9fbdfa1d5d375bcda7c24263c5fb4953584bf0
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-25 07:23:45 +0000

    設置場所変更に伴う、ファイルパス変更

--- Dollar/DollarTest.php
+++ Dollar/DollarTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src/Dollar.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Dollar.php';
 
 class DollarTest extends PHPUnit_Framework_TestCase
 {
