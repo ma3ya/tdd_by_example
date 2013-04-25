e1c5c16ed75e795c7af104e21f8c13f346d055b4
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-25 08:55:03 +0000

    amountのprivate化が完了

--- Dollar/Dollar.php
+++ Dollar/Dollar.php
@@ -10,7 +10,7 @@
  */
 class Dollar
 {
-    public $amount;
+    private $amount;
 
     public function __construct($amount)
     {
--- Dollar/DollarTest.php
+++ Dollar/DollarTest.php
@@ -9,7 +9,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'Dollar.php';
  * ====
  * - [ ] 2:1の場合 $5 + 10CHF = $10
  * - [x] $5 * 2 = $10
- * - [ ] amountのプライベート化
+ * - [x] amountのプライベート化
  * - [x] Dollarの副作用
  * - [ ] Moneyの丸め処理
  * - [x] equals()
