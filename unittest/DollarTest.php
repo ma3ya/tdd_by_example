141ed97e5a3390bbccc8a082fe13773bd45c3fa2
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-25 06:23:47 +0000

    乗法についてのテストを作成。

---
+++ unittest/DollarTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class DollarTest extends PHPUnit_Framework_TestCase
+{
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+        $five->times(2);
+        $this->assertSame(10, $five->amount);
+    }
+}
