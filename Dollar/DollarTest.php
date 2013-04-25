0a46b92346ee8c0c12c597335cd51ce57d88da70
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-25 07:28:44 +0000

    コメントにToDoリストを追加

--- Dollar/DollarTest.php
+++ Dollar/DollarTest.php
@@ -1,6 +1,19 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Dollar.php';
 
+/**
+ * Dollarクラスのテストコード。
+ *
+ * <pre>
+ * ToDo
+ * ====
+ * - [ ] 2:1の場合 $5 + 10CHF = $10
+ * - [x] $5 * 2 = $10
+ * - [ ] amountのプライベート化
+ * - [ ] Dollarの副作用
+ * - [ ] Moneyの丸め処理
+ * </pre>
+ */
 class DollarTest extends PHPUnit_Framework_TestCase
 {
     public function testMultiplication()
