dd57a4089410cc45c20c1162709382b30a68347d
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-27 07:24:33 +0000

    通貨(currency)取得機能のテストを追加

--- Money/MoneyTest.php
+++ Money/MoneyTest.php
@@ -24,4 +24,9 @@ class MoneyTest extends PHPUnit_Framework_TestCase
         $this->assertFalse(Money::franc(5)->equals(Money::franc(6)));
         $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
     }
+
+    public function testCurrency() {
+        $this->assertEquals("USD", Money::dollar(1)->currency());
+        $this->assertEquals("CHF", Money::franc(1)->currency());
+    }
 }
