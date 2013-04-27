22d194b2bf8430c169e3bfcfbca6023d32aab6b0
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-27 06:36:57 +0000

    テストケースのリファクタリング

--- Money/MoneyTest.php
+++ Money/MoneyTest.php
@@ -6,27 +6,22 @@ class MoneyTest extends PHPUnit_Framework_TestCase
 {
     public function testDollarMultiplication()
     {
-        $five = Money::dollar(5);
-        $this->assertEquals(Money::dollar(10), $five->times(2));
-        $this->assertEquals(Money::dollar(15), $five->times(3));
+        $this->assertEquals(Money::dollar(10), Money::dollar(5)->times(2));
+        $this->assertEquals(Money::dollar(15), Money::dollar(5)->times(3));
     }
 
     public function testFrancMultiplication()
     {
-        $five = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $five->times(2));
-        $this->assertEquals(Money::franc(15), $five->times(3));
+        $this->assertEquals(Money::franc(10), Money::franc(5)->times(2));
+        $this->assertEquals(Money::franc(15), Money::franc(5)->times(3));
     }
 
     public function testEquality()
     {
-        $dollar = Money::dollar(5);
-        $this->assertTrue($dollar->equals(Money::dollar(5)));
-        $this->assertFalse($dollar->equals(Money::dollar(6)));
-        $franc = Money::franc(5);
-        $this->assertTrue($franc->equals(Money::franc(5)));
-        $this->assertFalse($franc->equals(Money::franc(6)));
-
-        $this->assertFalse($franc->equals(Money::dollar(5)));
+        $this->assertTrue(Money::dollar(5)->equals(Money::dollar(5)));
+        $this->assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
+        $this->assertTrue(Money::franc(5)->equals(Money::franc(5)));
+        $this->assertFalse(Money::franc(5)->equals(Money::franc(6)));
+        $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
     }
 }
