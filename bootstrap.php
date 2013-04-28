e2fa409f90799fa92e951deae519644e49b1e953
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-28 10:01:27 +0000

    autoloadの読み込み順を変更

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
-
 function autoload($className)
 {
     $dir = __DIR__ . DIRECTORY_SEPARATOR . 'Money';
     $filepath = $dir . DIRECTORY_SEPARATOR . $className . '.php';
-    require_once $filepath;
+    include_once $filepath;
 }
 
 spl_autoload_register('autoload');
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
