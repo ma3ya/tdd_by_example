264fde641412258e47a5703c27ef31456f8359e0
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-29 09:29:46 +0000

    クラスの判別処理を除去

--- Money/Bank.php
+++ Money/Bank.php
@@ -4,11 +4,6 @@ class Bank
 {
     public function reduce(Expression $source, $to)
     {
-        if ($source instanceof Money) {
-            return $source->reduce($to);
-        }
-
-        $sum = $source;
-        return $sum->reduce($to);
+        return $source->reduce($to);
     }
 }
