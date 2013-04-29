2fd08d747cd7dd9b758ed16f772c82cdbef4720f
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-29 09:08:52 +0000

    plusメソッドがSumクラスを返すテストケースを追加

--- Money/MoneyTest.php
+++ Money/MoneyTest.php
@@ -29,4 +29,13 @@ class MoneyTest extends PHPUnit_Framework_TestCase
         $reduced = $bank->reduce($sum, "USD");
         $this->assertEquals(Money::dollar(10), $reduced);
     }
+
+    public function testPlusReturnsSum()
+    {
+        $five = Money::dollar(5);
+        $result = $five->plus($five);
+        $sum = $result;
+        $this->assertEquals($five, $sum->augend);
+        $this->assertEquals($five, $sum->addend);
+    }
 }
