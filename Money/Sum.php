e3f96d5207271d749427602caf839ddc11094cbd
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-29 09:11:35 +0000

    テストをパスするように改修
    - Sumクラスを追加
    - Money::plusの戻り値の型を変更
    - Bankのreduce処理を実装

---
+++ Money/Sum.php
@@ -0,0 +1,13 @@
+<?php
+
+class Sum implements Expression
+{
+    public $augend;
+    public $addend;
+
+    public function __construct(Money $augend, Money $addend)
+    {
+        $this->augend = $augend;
+        $this->addend = $addend;
+    }
+}
