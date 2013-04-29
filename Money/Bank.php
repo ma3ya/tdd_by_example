3195a2e8d99355f7fddc1afd2d42e07a335b4206
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-29 09:26:17 +0000

    テストがパスするように改修

--- Money/Bank.php
+++ Money/Bank.php
@@ -4,6 +4,10 @@ class Bank
 {
     public function reduce(Expression $source, $to)
     {
+        if ($source instanceof Money) {
+            return $source;
+        }
+
         $sum = $source;
         return $sum->reduce($to);
     }
