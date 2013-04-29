a04838aa118eea54edd53441cf4f409d9765f286
Masaya Ozawa <ma3ya.ozw@gmail.com>
2013-04-29 10:40:29 +0000

    rateをBankに移動

--- Money/Bank.php
+++ Money/Bank.php
@@ -10,4 +10,9 @@ class Bank
     public function addRate($from, $to, $rate)
     {
     }
+
+    public function rate($from, $to)
+    {
+        return ($from === "CHF" && $to === "USD") ? 2 : 1;
+    }
 }
